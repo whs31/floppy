d6f523622669916655e1e6e10ee11b2470b92324
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2025-03-27 21:26:03 +0300

    Version bump

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.files import rmdir, copy
 
 class RollyRecipe(ConanFile):
     name = "rolly"
-    version = "2.4.6"
+    version = "2.4.7"
     description = "Radar open-source library"
     author = "whs31 <whs31@github.io>"
     topics = ("coreutils", "utility")
