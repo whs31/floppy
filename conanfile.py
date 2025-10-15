772904f833d1ccf344b67022e3660a8960ca0a8e
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2025-10-15 16:40:12 +0300

    v2.7.5

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.files import rmdir, copy
 
 class RollyRecipe(ConanFile):
     name = "rolly"
-    version = "2.7.4"
+    version = "2.7.5"
     description = "Radar open-source library"
     author = "whs31 <whs31@github.io>"
     topics = ("coreutils", "utility")
@@ -17,6 +17,9 @@ class RollyRecipe(ConanFile):
     default_options = {"shared": True, "test": False}
     exports = "CMakeLists.txt", "conanfile.py"
     exports_sources = "*", "!build/*"
+
+    user = "radar"
+    channel = "dev"
 
     @property
     def _min_cppstd(self):
