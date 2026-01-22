b0bbab23c0b67acc47a5d0df5ed7e3a7ecf89190
whs31 <riazantsev.dl@edu.spbstu.ru>
2026-01-22 16:45:30 +0300

    v2.10.0

--- include/rll/all.h
+++ include/rll/all.h
@@ -6,12 +6,10 @@
 #include <rll/contracts.h>
 #include <rll/crypto.h>
 #include <rll/deadline_timer.h>
-#include <rll/dlfcn.h>
 #include <rll/elapsed_timer.h>
 #include <rll/fixed_string.h>
 #include <rll/functional.h>
 #include <rll/global.h>
-#include <rll/library.h>
 #include <rll/lockfile.h>
 #include <rll/math.h>
 #include <rll/memory.h>
